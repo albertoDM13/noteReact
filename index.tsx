5c27b77b47f5f86752fbececc111e05de5a337bf
albertoDM13 <jesus_duk_12@hotmail.com>
2021-05-09 19:14:07 -0500

    2 files modified

--- index.tsx
+++ index.tsx
@@ -51,7 +51,7 @@ class App extends Component<AppProps, AppState> {
               <div className="LTR"></div>
           </div>
         </div>
-      </div>//  class .body
+      </div>
     );
   }
 }
